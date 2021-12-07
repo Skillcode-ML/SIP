ce7b080d4b14e53f61880b48291e7206cb131633
Skillcode-ML <84472319+Skillcode-ML@users.noreply.github.com>
2021-12-08 01:12:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ rate=st.number_input('Rate', 1)
 time=st.number_input('Time', 1)
 princ=st.number_input('Principal Amount', 1)
 month=st.checkbox("In Months")
-if month:
-    st.write(sip(pamoun, time, rate, princ, False))
-else:
-    st.write(sip(pamoun, time, rate, princ))
 
 def sip(investment, tenure, interest, amount=0, is_year=True, is_percent=True, show_amount_list=True):
     tenure = tenure*12 if is_year else tenure
@@ -19,3 +15,8 @@ def sip(investment, tenure, interest, amount=0, is_year=True, is_percent=True, s
         amount = (amount + investment)*(1+interest)
         amount_every_month[month+1] = amount
     return {'Amount @ Maturity': amount, 'Amount every month': amount_every_month} if show_amount_list else {'Amount @ Maturity': amount} 
+
+if month:
+    st.write(sip(pamoun, time, rate, princ, False))
+else:
+    st.write(sip(pamoun, time, rate, princ))
